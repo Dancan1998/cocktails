c6a004de443264ff8167dc0bcda48d6d438571b5
Dancan Chibole <dancankingstar@gmail.com>
2020-12-28 15:22:05 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import pages
 
+// import components
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Error from "./pages/Error";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/cocktail/:id">
+          <SingleCocktail />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+        laboriosam illum sequi quas atque quos nobis optio, similique, quaerat
+        quod delectus debitis mollitia soluta. Ex, excepturi similique. Quaerat
+        facere, unde quas dolorem quibusdam quisquam ad eos ullam veniam
+        incidunt aperiam.
+      </p>
+    </section>
+  );
+};
+
+export default About;
